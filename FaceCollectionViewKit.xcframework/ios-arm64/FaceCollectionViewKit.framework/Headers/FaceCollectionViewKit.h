a43b6dc781239b9b06aebf1e5cd7db0749c06879
serhat akalin <serhatakalin@lyrebirdstudio.net>
2022-08-30 05:54:29 +0300

    add xcframework

---
+++ FaceCollectionViewKit.xcframework/ios-arm64/FaceCollectionViewKit.framework/Headers/FaceCollectionViewKit.h
@@ -0,0 +1,18 @@
+//
+//  FaceCollectionViewKit.h
+//  FaceCollectionViewKit
+//
+//  Created by serhat akalin on 30.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceCollectionViewKit.
+FOUNDATION_EXPORT double FaceCollectionViewKitVersionNumber;
+
+//! Project version string for FaceCollectionViewKit.
+FOUNDATION_EXPORT const unsigned char FaceCollectionViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceCollectionViewKit/PublicHeader.h>
+
+
